7ed783fbef7d634a1fd7392cc9040ae53d6bce1b
Helvecios <helvecios@globo.com>
2020-10-20 20:09:18 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,8 +16,12 @@ public class Main extends Application {
 		try {
 			//Instanciar um novo objeto "loader" do tipo FXMLLoader
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load(); //Chama o loader.load() para carregar a view
-			Scene mainScene = new Scene(parent); //Cena principal
+			ScrollPane scrollPane = loader.load(); //Chama o loader.load() para carregar a view
+			
+			scrollPane.setFitToHeight(true); //Para o menu se ajustar automaticamente na altura da tela
+			scrollPane.setFitToWidth(true); //Para o menu se ajustar automaticamente na largura da tela
+			
+			Scene mainScene = new Scene(scrollPane); //Cena principal
 			primaryStage.setScene(mainScene); //Palco da cena
 			primaryStage.setTitle("Sample JavaFX application"); //Título do palco
 			primaryStage.show(); //Mostra o palco
